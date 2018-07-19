f849c3a11f39dbd325dc25d0f6334cc3131d902d
saichaturvedula1 <30464548+saichaturvedula1@users.noreply.github.com>
2018-07-20 00:32:59 +0530

    first commit

---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent {
+  colSpanValue:number = 2;
+  nameStyle:string = "nameClass";
+  hasAnotherClass:boolean = true;
+  ageColor:string = "rgb(40, 128, 138)";
+
+  fName:string = "Narella";
+  lName:string = "Rao";
+  empCode:string = "P0077";
+  empAge:string = "25";
+  company:string = "Hurix";
+  empName():string {
+    return this.fName+" "+this.lName;
+  }
+
+  buttonText:string = "Show Details";
+  isTableDisplayed:boolean = false;
+
+  showTableData(): void{
+    this.isTableDisplayed = !this.isTableDisplayed;
+    this.buttonText = (this.isTableDisplayed == true) ? "Hide Details" : "Show Details";
+    // return buttonText
+  }
+
+}
